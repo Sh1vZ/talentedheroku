3880d4cf5d9bd121e156cfaeba95ba977b158ffb
Inv Technologies <37981347+Inv-Technologies@users.noreply.github.com>
2018-04-10 15:12:50 -0400

    Add files via upload

---
+++ static/welcome.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+<title>Cleveland Cavaliers</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="icon" href="cavs2.png">
+<style>
+body {
+   background-color: darkred;
+}
+h1 {
+  color: yellow;
+}
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    max-width: 300px;
+    margin: auto;
+    text-align: center;
+}
+
+.title {
+    color: grey;
+    font-size: 18px;
+}
+
+button {
+    border: none;
+    outline: 0;
+    display: inline-block;
+    padding: 8px;
+    color: white;
+    background-color: #000;
+    text-align: center;
+    cursor: pointer;
+    width: 100%;
+    font-size: 18px;
+}
+
+a {
+    text-decoration: none;
+    font-size: 22px;
+    color: black;
+}
+
+button:hover, a:hover {
+    opacity: 0.7;
+}
+</style>
+</head>
+<body>
+
+<h1><center>Welcome to Cleveland Cavaliers infodump site. <center></h1>
+
+<div class="card">
+  <img src="lebron.jpg" alt="Lebron James" style="width:100%">
+  <h1>Lebron James</h1>
+  <p class="title">Number 23</p>
+  <p>Position: Small Forward, Power Forward</p>
+  <p>Cleveland Cavaliers</p>
+  <a href="#"><i class="fa fa-google"></i></a> 
+  <a href="#"><i class="fa fa-twitter"></i></a> 
+  <a href="#"><i class="fa fa-linkedin"></i></a> 
+  <a href="#"><i class="fa fa-facebook"></i></a> 
+  <p><button>Lebron's profile</button></p>
+</div>
+<center>
+<div class="card">
+  <img src="kevin.jpg" alt="Kevin Love" style="width:100%">
+  <h1>Kevin Love</h1>
+  <p class="title">Number 0</p>
+  <p>Position: Center, Power Forward</p>
+  <p>Cleveland Cavaliers</p>
+  <a href="#"><i class="fa fa-google"></i></a> 
+  <a href="#"><i class="fa fa-twitter"></i></a> 
+  <a href="#"><i class="fa fa-linkedin"></i></a> 
+  <a href="#"><i class="fa fa-facebook"></i></a> 
+  <p><button>Kevin's profile</button></p>
+</div>
+</center>
+
+
+</body>
+</html>
