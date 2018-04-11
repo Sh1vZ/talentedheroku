0b48c215bd52f4737611780ce1305259b6013aa7
Inv Technologies <37981347+Inv-Technologies@users.noreply.github.com>
2018-04-10 21:16:46 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/')
+@app.route('/login')
 def home():
     return render_template('index.html')
+
+@app.route('/login')
+def home():
+    return render_template('signup.html')
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
